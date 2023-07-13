3e07a81667137fe08e200da117284dbb95f704df
Arthur Di Lascio <31333091+adlascio@users.noreply.github.com>
2023-07-13 15:39:33 -0700

    add solution recursion

--- recursion.js
+++ recursion.js
@@ -1,6 +1,13 @@
 //Print only items (emojis) in the console. Not the arrays.
 
-const printItems = function (array) {};
+const printItems = function (array) {
+  for(const el of array){
+    if(Array.isArray(el)){
+      printItems(el);
+    }
+    console.log(el);
+  }
+};
 
 const array = ["😎", "💩", "🤗", "😼", "😂"];
 printItems(array);
